25f6a978a71678f94c6c1de4a07893606330982b
Vignan Digoju(UST <290583@ust.com>
2026-01-19 11:21:49 +0530

    updates

--- app/db/models.py
+++ app/db/models.py
@@ -1,12 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, conint
 from typing import List
 
-
 class Answer(BaseModel):
-    question_id: int
-    alternative_id: int
-
+    question_id: conint(ge=0, le=2147483647)
+    alternative_id: conint(ge=0, le=2147483647)
 
 class UserAnswer(BaseModel):
-    user_id: int
+    user_id: conint(ge=0, le=2147483647)
     answers: List[Answer]
